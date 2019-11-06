a00ffd2827743b954c7ca2f30c1387414bb1a093
echo <306327680@qq.com>
2019-11-06 14:30:43 +0800

    with out numeical diff

--- main.cpp
+++ main.cpp
@@ -6,18 +6,24 @@
 #include "LMicp.h"
 int main() {
 	//整个地图
-	std::string _whole_map = "/home/echo/CLionProjects/LMICP/data/pcd/1551690896.751801000.pcd";
+	std::string _whole_map = "/home/echo/log/map.pcd";
+	//std::string _whole_map = "/home/echo/CLionProjects/LMICP/data/map/project.pcd";
+	
+
 	//当前扫描的点云
-	std::string cloud_frame = "/home/echo/CLionProjects/LMICP/data/map/project.pcd";
+	std::string cloud_frame = "/home/echo/CLionProjects/LMICP/data/curr/moved.pcd";
 	//设置初始的位姿
 	Eigen::Isometry3d raw_pose = Eigen::Isometry3d::Identity();
 	pcl::PointCloud<pcl::PointXYZI>target_cloud;
 	pcl::PointCloud<pcl::PointXYZI> source;
-	pcl::io::loadPCDFile<pcl::PointXYZI>(_whole_map, target_cloud);
+	pcl::PointCloud<pcl::PointXYZI>::Ptr fast_map(new pcl::PointCloud<pcl::PointXYZI>);
+	pcl::io::loadPCDFile<pcl::PointXYZI>(_whole_map, *fast_map);
 	pcl::io::loadPCDFile<pcl::PointXYZI>(cloud_frame, source);
-	LMicp lm;
-	lm.timeCalcSet("lm");
-	lm.solveICP(target_cloud,source,raw_pose);
-	lm.timeUsed();
+
+
+	LMicp lm,a;
+	a.timeCalcSet("lm");
+	lm.solveICP(fast_map,source,raw_pose);
+	a.timeUsed();
 	return 0;
 }
